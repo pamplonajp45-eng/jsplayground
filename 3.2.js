d63263dd2d3c4cf905f0fa608db89390446620e8
JPDEV <pamplonajp.45@gmail.com>
2026-07-25 09:30:50 +0800

    module 2.3 - 3

---
+++ 3.2.js
@@ -0,0 +1,7 @@
+let width = prompt("Volume of the box, enter width", 20);
+let height = prompt("Volume of the box, enter height", 10);
+let length = prompt("Volume of the box, enter length", 50);
+let volume = width * height * length;
+alert(`Calculated box volume is ${volume}`);
+// let decision = window.confirm("Is it OK?");
+// console.log(decision);
