c1ecb3754c0c69f505758f720af783e5141b09f2
JPDEV <pamplonajp.45@gmail.com>
2026-08-05 12:28:06 +0800

    functions

--- cisco/module_5.js
+++ cisco/module_5.js
@@ -11,3 +11,18 @@ function getMeanTemp(temps) {
 }
 const meanTemps = getMeanTemp([22, 24, 19, 21, 23]);
 console.log(`Mean temperature: ${meanTemps}`);
+
+// functions are declared before they are called,
+
+let name = "Jordan";
+function showName() {
+  console.log(name);
+}
+showName(); // This will work because the function is hoisted
+
+// This is also work the same
+let name = "Jordan";
+showName();
+function showName() {
+  console.log(name);
+}
